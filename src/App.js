5dc1200a2180f25bb0c5c0bb755a5d04cce80e1d
Cato <roaldsoyen@gmail.com>
2023-02-02 22:35:10 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import "./styles/styles.scss";
+import NavMenu from "./components/menu/NavMenu";
+import { AdminContextProvider } from "./context/AdminContext";
+import { foodMenu } from './constants/foodMenu';
+
 
 function App() {
+
+  function setLocalStorage() {
+    localStorage.setItem("foodMenu", JSON.stringify(foodMenu))
+  }
+
+  setLocalStorage();
+
   return (
     <div className="App">
-     
+     <AdminContextProvider>
+      <NavMenu />
+     </AdminContextProvider>
     </div>
   );
 }
