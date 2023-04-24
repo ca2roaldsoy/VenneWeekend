1bfaf3d5dc7f349ae9230459ad682b9064afaaab
Cato <roaldsoyen@gmail.com>
2023-04-24 12:10:18 +0200

    added map

---
+++ src/components/map/Map.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Map() {
+  return (
+    <>
+    <h1>Kart</h1>
+    <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d8356.455327744026!2d6.4419325!3d58.4251985!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4639efa7acc714a9%3A0x650330211c9f3997!2sLunden%20Leir-%20og%20Fritidssenter!5e0!3m2!1sen!2sno!4v1682330117557!5m2!1sen!2sno" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    </>
+
+  )
+}
+
+export default Map
