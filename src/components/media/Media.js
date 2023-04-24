260f2537da8c1fbd3321e2e65537e9429319d6b6
Cato <roaldsoyen@gmail.com>
2023-04-24 12:11:33 +0200

    fixed media to database

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -1,62 +1,54 @@
-import React from 'react';
-import ImageUploading from 'react-images-uploading';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { Button } from 'react-bootstrap';
 
 export function Media() {
-  const [images, setImages] = React.useState([]);
-  const maxNumber = 100;
+      const [file, setFile] = useState();
+      const [fileName, setFileName] = useState("");
+      const [data, setData] = useState([])
+ 
+      const saveFile = (e) => {
+        setFile(e.target.files[0]);
+        setFileName(e.target.files[0].name);
+      };
 
-  const onChange = (imageList, addUpdateIndex) => {
-    // data for submit
-    setImages(imageList);
-  };
+      
+      useEffect(() => {
+        axios.get("http://localhost:3001/media")
+      .then(res =>  setData(res.data))
+      .catch(err => console.log(err))
+    }, [])
+ 
+      const uploadFile = () => {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("fileName", fileName);
+        axios.post("http://localhost:3001/media", formData).then(window.location.href = "/media")
+      };
 
-  return (
-    <div className="App">
-      <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageRemoveAll,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          // write your building UI
-          <div className="upload__image-wrapper">
-            <button
-              style={isDragging ? { color: 'red' } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-              className="btn btn-primary"
-            >
-              Last opp bildet
-            </button>
-            &nbsp;
-            <button onClick={onImageRemoveAll} className="btn btn-danger">Slett alle bildene</button>
-            {imageList.map((image, index) => (
-              <div key={index} className="image-item">
-                <h3>2022</h3>
-                <hr />
-                <img src={image['data_url']} alt="" width="100" />
-                <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Oppdater</button>
-                  <button onClick={() => onImageRemove(index)}>Slett</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </ImageUploading>
-    </div>
-  );
+    const deleteFile = (id) => {
+     const newData = [...data];
+      const index = data.findIndex((d) => d.id === id);
 
+      console.log(data);
+
+      newData.splice(index, 1);
+      axios.delete(`http://localhost:3001/media/delete/${id}`)
+      setData(newData);
+    }
+
+      return (
+        <div>
+            <input type="file" onChange={saveFile} />
+            <Button onClick={() => uploadFile()}>Upload</Button>
+            {data.map((d, index) => (
+              <div key={index} className="pb-3">
+               <img src={"http://localhost:3001/images/"+d.image} alt="" />
+               <Button onClick={() => deleteFile(d.id)}>delete</Button>
+            </div>
+           ))}
+        </div>
+      );
 
 }
 export default Media
