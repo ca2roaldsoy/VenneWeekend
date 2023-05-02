d7dac06f4d57a3b7ccb19a89be145f62dfdc4ab0
Cato <roaldsoyen@gmail.com>
2023-05-02 22:10:35 +0200

    fixed media

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -1,32 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button } from 'react-bootstrap';
+import { ImageGroup, Image } from "react-fullscreen-image";
 
 export function Media() {
-      const [file, setFile] = useState();
-      const [fileName, setFileName] = useState("");
-      const [data, setData] = useState([])
- 
-      const saveFile = (e) => {
-        setFile(e.target.files[0]);
-        setFileName(e.target.files[0].name);
-      };
+  const [files, setFiles] = useState([]);
 
-      
-      useEffect(() => {
-        axios.get("http://localhost:3001/media")
-      .then(res =>  setData(res.data))
-      .catch(err => console.log(err))
-    }, [])
- 
-      const uploadFile = () => {
-        const formData = new FormData();
-        formData.append("file", file);
-        formData.append("fileName", fileName);
-        axios.post("http://localhost:3001/media", formData).then(window.location.href = "/media")
-      };
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/media/")
+      .then((res) => {
+        setFiles(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    const deleteFile = (id) => {
+  const setimgfile = (e) => {
+    setFiles(e.target.files);
+  };
+
+  const addUserData = (event) => {
+    event.preventDefault();
+    var formData = new FormData();
+
+    for (const file of files) {
+      formData.append("files", file);
+    }
+
+    const res = axios
+      .post("http://localhost:3001/media", formData)
+      .then((window.location.href = "./media"));
+
+    if (res.data.status === 201) {
+      console.log("NO error");
+    } else {
+      console.log("error");
+    }
+  };
+
+  const fileList = files ? [...files] : [];
+
+  /* const deleteFile = (id) => {
      const newData = [...data];
       const index = data.findIndex((d) => d.id === id);
 
@@ -35,20 +48,51 @@ export function Media() {
       newData.splice(index, 1);
       axios.delete(`http://localhost:3001/media/delete/${id}`)
       setData(newData);
-    }
+    } */
 
-      return (
-        <div>
-            <input type="file" onChange={saveFile} />
-            <Button onClick={() => uploadFile()}>Upload</Button>
-            {data.map((d, index) => (
-              <div key={index} className="pb-3">
-               <img src={"http://localhost:3001/images/"+d.image} alt="" />
-               <Button onClick={() => deleteFile(d.id)}>delete</Button>
-            </div>
-           ))}
-        </div>
-      );
-
+  return (
+    <>
+      <div style={{ padding: 15, backgroundColor: "#fff", marginTop: 15 }}>
+        <h4>Upload file:</h4>
+        <input type="file" name="photo" onChange={setimgfile} multiple />
+        <button type="submit" onClick={addUserData}>
+          submit
+        </button>
+      </div>
+      <ImageGroup>
+        <ul
+          className="images"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+            gridGap: "15px",
+            listStyleType: "none",
+            margin: 0,
+            padding: 0,
+          }}
+        >
+          {fileList.map((file, i) => (
+            <li key={i} style={{ position: "relative", paddingTop: "66%" }}>
+              <Image
+                key={file.id}
+                src={"http://localhost:3001/images/" + file.image}
+                alt={file.name}
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  height: "100%",
+                  width: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </ImageGroup>
+    </>
+  );
 }
-export default Media
+export default Media;
