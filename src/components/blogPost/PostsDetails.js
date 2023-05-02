d8775d67298c4bdfa303193bac1af438bc57c062
Cato <roaldsoyen@gmail.com>
2023-05-02 22:11:47 +0200

    cleanup

--- src/components/blogPost/PostsDetails.js
+++ src/components/blogPost/PostsDetails.js
@@ -4,25 +4,23 @@ import axios from 'axios';
 
 function PostsDetails() {
 
-const { title, id, message } = useParams(); 
+const { title, id } = useParams(); 
 
 const [posts, setPosts] = useState([])
 
 useEffect(() => {
      axios.get("http://localhost:3001/post/get/", {
         params: { 
-            id: id,
-
+            id: id
         }
      }).then((response) =>{
-       console.log(response.data);
         setPosts(response.data)
     })
 }, []);
 
 return (
     <>
-        <h1>{title}</h1>
+      <h1>{title}</h1>
        {posts.map(p => {
         const numId = parseInt(id)
           if(p.id === numId) {
