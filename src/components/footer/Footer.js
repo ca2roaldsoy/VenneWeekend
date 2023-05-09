4b08e1f3405e2a49d59247e9fbca3f824aa9a813
Cato <roaldsoyen@gmail.com>
2023-05-09 23:59:52 +0200

    add footer elements

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import FacebookIcon from "../../images/face_v1.png";
+import { Col, Row, Image } from "react-bootstrap";
+import FooterMenu from "./FooterMenu";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <Row as="footer" role="contentinfo" className="footer">
+      <Col sm={12} md={3} lg={3} as="section" className="footer__title">
+        <h1 className="footer__title--text">Holidaze</h1>
+      </Col>
+      <Col sm={12} md={3} lg={3} as="section" className="footer__contact">
+        <FooterMenu />
+      </Col>
+      <Col sm={12} md={3} lg={3} as="section" className="footer__social">
+        <a
+          href="https://www.facebook.com/groups/449296311930329"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Image src={FacebookIcon} alt="facebook" fluid role="link" />
+        </a>
+      </Col>
+      <Col sm={12} md={3} lg={3} as="section" className="footer__copy">
+        <p>2023 VenneWeekend</p>
+        <p>Lunden Lunden leir- og fritidssenter</p>
+      </Col>
+    </Row>
+  );
 }
 
-export default Footer
+export default Footer;
