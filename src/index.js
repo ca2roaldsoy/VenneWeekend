f9bbfc0e5858cbaacd1d5abfb4d1c57647c9429f
Cato <roaldsoyen@gmail.com>
2023-05-18 00:47:53 +0200

    remove query client

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const queryClient = new QueryClient()
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+{
+  /* <React.StrictMode>  */
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-    );
-{/* <React.StrictMode>  */}
-
-{/*   </React.StrictMode>  */}
-
+{
+  /*   </React.StrictMode>  */
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
