4d71c63e3281604c82d847069129e8bcac3d4f10
Cato <roaldsoyen@gmail.com>
2023-05-22 00:07:26 +0200

    registration

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Registration from "../registration/Registration";
 import LoginForm from "./LoginForm";
 
 // Navigation Menu
 function Login() {
-
   return (
     <>
       <h1>Logg Inn</h1>
-          <LoginForm />
+      <Registration />
+      <LoginForm />
     </>
   );
 }
